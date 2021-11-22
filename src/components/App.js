0867588f8cd8416101a011a0cc9d1c98178944d5
Meg <meganriley806@gmail.com>
2021-11-21 18:28:13 -0600

    updated app.js file

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import Movie from "./Movie";
 import Search from "./Search";
 
 
-const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b";
+const MOVIE_API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=5da5b498";
 
 
 const initialState = {
